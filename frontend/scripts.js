b13b76022d5f9cd2b65cdbda6039285cdf2cd0d6
Magnus.Scherrmann <magnus.scherrmann@nagarro-es.com>
2024-02-23 12:15:13 +0100

    first commit

---
+++ frontend/scripts.js
@@ -0,0 +1,7 @@
+
+import * as calendar from '../calendar-js-2.10.12/dist/calendar.js'
+
+var calendarInstance1 = new calendarJs("calendar", {
+    manualEditingEnabled: true
+    // All your options can be set here
+});
