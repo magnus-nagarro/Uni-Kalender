bd582d705f8640818896c0861250f264529e6056
AmirGharbi <gharbiamir40@gmail.com>
2024-03-03 20:48:35 +0100

    info button, advantages icons, blank space

--- frontend/Start-site/script.js
+++ frontend/Start-site/script.js
@@ -23,6 +23,21 @@ function autoChangeSlide() {
     changeSlide(currentSlideIndex);
 }
 
-let slideInterval = setInterval(autoChangeSlide, 5000); // Change slide every 2 seconds
+let slideInterval = setInterval(autoChangeSlide, 5000);
 
 changeSlide(0);
+
+
+let currentIconIndex = 0;
+
+function changeInformation(direction) {
+    const iconContainers = document.querySelectorAll('.icon-container');
+    iconContainers[currentIconIndex].classList.add('hide');
+    currentIconIndex += direction;
+    if (currentIconIndex < 0) {
+        currentIconIndex = iconContainers.length - 1;
+    } else if (currentIconIndex >= iconContainers.length) {
+        currentIconIndex = 0;
+    }
+    iconContainers[currentIconIndex].classList.remove('hide');
+}
