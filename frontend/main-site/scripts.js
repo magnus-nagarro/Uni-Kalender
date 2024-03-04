d5adb1f59741f8b703d15cbaf459c36d968a6180
Magnus.Scherrmann <magnus.scherrmann@nagarro-es.com>
2024-03-04 10:38:53 +0100

    Added some Event listeners

--- frontend/main-site/scripts.js
+++ frontend/main-site/scripts.js
@@ -1,4 +1,3 @@
-
 import * as calendar from '../../calendar-js-2.10.12/dist/calendar.js'
 
 var calendarInstance1 = new calendarJs("calendar", {
@@ -21,6 +20,46 @@ var calendarInstance1 = new calendarJs("calendar", {
             showExtraTitleBarButtons: false
         }
     },
-});
 
+    //Add Event data in the backend
+    onEventAdded: function (event) {
+        console.log("Event added");
+        var json_event = JSON.stringify(event);
+        console.log(json_event);
+        fetch('http://127.0.0.1:9090/lecturedates', {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: json_event,
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                console.log('Event data saved successfully.');
+            })
+            .catch(error => {
+                console.error('Error saving event data:', error);
+            });
+    },
+
+    //Update Event Data in the backend
+    onEventUpdated: function (event) {
+        console.log("Event updated");
+        var updated_data = JSON.stringify(event);
+        console.log(updated_data);
+    },
+
+    //Remove Events from the backend
+    onEventRemoved: function (event) {
+        console.log("Event deleted");
+    },
+
+    //Pull Events from the backend and import them from json
+    onRender: function (event) {
+        console.log("Calendar rendered");
+    }
+});
 
