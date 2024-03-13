6ef291714f9b77658ad8c6addb99a54b4a55a593
Magnus.Scherrmann <magnus.scherrmann@nagarro-es.com>
2024-03-13 09:56:37 +0100

    backend fully functional

--- backend/main.py
+++ backend/main.py
@@ -31,8 +31,36 @@ class Lecture():
         self.created = created
         self.lastUpdated = lastUpdated
 
-    def return_info_json():
-        pass
+    def return_info_json(self):
+        return {
+            "from": self.start,
+            "to": self.end,
+            "title": self.title,
+            "description": self.description,
+            "location": self.location,
+            "group": self.group,
+            "isAllDay": self.isAllDay,
+            "showAlerts": self.showAlerts,
+            "showAsBusy": self.showAsBusy,
+            "color": self.color,
+            "colorText": self.colorText,
+            "colorBorder": self.colorBorder,
+            "repeatEveryExcludeDays": self.repeatEveryExcludeDays,
+            "repeatEnds": self.repeatEnds,
+            "url": self.url,
+            "repeatEveryCustomValue": self.repeatEveryCustomValue,
+            "repeatEvery": self.repeatEvery,
+            "repeatEveryCustomType": self.repeatEveryCustomType,
+            "organizerName": self.organizerName,
+            "organizerEmailAddress": self.organizerEmail,
+            "type": self.type,
+            "locked": self.locked,
+            "costumTags": self.costumTags,
+            "alertOffset": self.alertOffset,
+            "id": self.id,
+            "created": self.created,
+            "lastUpdated": self.lastUpdated,
+        }
 
 
 class Lecturer():
@@ -42,16 +70,19 @@ class Lecturer():
 
     def return_info_json(self):
         return {
-            "name": self.name,
+            "name": self.name[0],
             "e_mail": self.e_mail
         }
+
+    def get_name(self):
+        return self.name[0]
 
 
 class Backend():
     def __init__(self) -> None:
         self.lectures = list()
         self.lecturers = list()
-        self.current_user = Lecturer()
+        self.current_user = None
 
     def create_app(self):
         app = Flask(__name__)
@@ -63,18 +94,32 @@ class Backend():
                             "message": "Hello World"})
 
         # GET mit /lecture
-        # POST mit den Eventdaten als Body
+        # POST mit den Eventdaten als Body, erstellt neue lectures und updated bestehende
         @app.route('/lecture', methods=["POST", "GET"])
         def lectures():
             if request.method == "POST":
                 try:
                     json_event = request.get_json()
+                    if self.current_user == None:
+                        return jsonify({"success": False,
+                                        "message": "User not found!"})
+                    for lecture in self.lectures:
+                        if lecture.id == json_event["id"]:
+                            lecture = Lecture(json_event['from'], json_event['to'], json_event['title'], json_event['description'], json_event['location'], json_event['group'], json_event['isAllDay'],
+                                              json_event['showAlerts'], json_event['showAsBusy'], json_event['color'], json_event[
+                                              'colorText'], json_event['colorBorder'], json_event['repeatEveryExcludeDays'],
+                                              json_event['repeatEnds'], json_event['url'], json_event[
+                                              'repeatEveryCustomValue'], json_event['repeatEvery'], json_event['repeatEveryCustomType'],
+                                              self.current_user.name[0], self.current_user.e_mail, json_event[
+                                              'type'], json_event['locked'], json_event['customTags'], json_event['alertOffset'],
+                                              json_event['id'], json_event['created'], json_event['lastUpdated'])
+
                     new_lecture = Lecture(json_event['from'], json_event['to'], json_event['title'], json_event['description'], json_event['location'], json_event['group'], json_event['isAllDay'],
                                           json_event['showAlerts'], json_event['showAsBusy'], json_event['color'], json_event[
                                               'colorText'], json_event['colorBorder'], json_event['repeatEveryExcludeDays'],
                                           json_event['repeatEnds'], json_event['url'], json_event[
                                               'repeatEveryCustomValue'], json_event['repeatEvery'], json_event['repeatEveryCustomType'],
-                                          self.current_user.name, self.current_user.e_mail, json_event[
+                                          self.current_user.name[0], self.current_user.e_mail, json_event[
                                               'type'], json_event['locked'], json_event['customTags'], json_event['alertOffset'],
                                           json_event['id'], json_event['created'], json_event['lastUpdated'])
                     self.lectures.append(new_lecture)
@@ -85,9 +130,18 @@ class Backend():
                     print(e)
                     return f"Error processing event data: {e}", 500
             elif request.method == "GET":
+                return_dict = dict()
+                counter = int()
+                if self.current_user == None:
+                    return jsonify({"success": False,
+                                    "message": "User not found!"})
                 for lecture in self.lectures:
-                    if lecture['organizerName'] == self.current_user.name:
-                        pass
+                    print(lecture.organizerName)
+                    print(self.current_user.name[0])
+                    if lecture.organizerName == self.current_user.name[0]:
+                        counter += 1
+                        return_dict[counter] = lecture.return_info_json()
+                return jsonify(return_dict)
 
         # GET mit /lecturer?name=""
         # POST mit den Dozentendaten als Body
@@ -97,9 +151,8 @@ class Backend():
                 try:
                     json_lecturer = request.get_json()
                     new_lecturer = Lecturer(
-                        json_lecturer['name'], json_lecturer['e_mail'])
+                        json_lecturer['name'], json_lecturer['e-mail'])
                     self.lecturers.append(new_lecturer)
-                    print(self.lecturers)
                     return jsonify({"success": True,
                                     "message": "Added lecturer to the list"})
                 except Exception as e:
@@ -113,7 +166,7 @@ class Backend():
         def sign_in():
             name = request.args.get('name')
             for lecturer in self.lecturers:
-                if lecturer.name == name:
+                if lecturer.get_name() == name:
                     self.current_user = lecturer
                     return jsonify(lecturer.return_info_json())
             return jsonify({
